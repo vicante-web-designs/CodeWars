d1039fce3193eabdf51cb7c0de0f953efe0aa26b
Batubo Victory <victorybatubo76@gmail.com>
2026-01-06 14:47:58 +0100

    feat: Kata solved

--- src/js/script.js
+++ src/js/script.js
@@ -634,7 +634,39 @@ Input = "The sunset sets at twelve o' clock."
 Output = "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" */
 
 function alphabetPosition(text) {
-  return text;
+  const alphabet = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4,
+  e: 5,
+  f: 6,
+  g: 7,
+  h: 8,
+  i: 9,
+  j: 10,
+  k: 11,
+  l: 12,
+  m: 13,
+  n: 14,
+  o: 15,
+  p: 16,
+  q: 17,
+  r: 18,
+  s: 19,
+  t: 20,
+  u: 21,
+  v: 22,
+  w: 23,
+  x: 24,
+  y: 25,
+  z: 26
+};
+
+  const splitText = text.toLowerCase().replace(/\s+/g, "").split('')
+  const numeric = splitText.map(letter => alphabet[letter])
+
+  return numeric.filter(num => num !== undefined).join(' ')
 }
 
-console.log(alphabetPosition('Hello'));
+console.log(alphabetPosition("The sunset sets at twelve o' clock."))
