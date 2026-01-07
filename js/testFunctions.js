6a674fb3e63065ac9f5ed6fe50c5ec3a26048151
Batubo Victory <victorybatubo76@gmail.com>
2026-01-07 09:23:31 +0100

    feat: Daily log solved

--- js/testFunctions.js
+++ js/testFunctions.js
@@ -16,6 +16,25 @@ Input may be any positive or negative integer (including 0).
 
 You can assume that all inputs are valid integers.*/
 
+
+//My solution
 function roundToNext5(n){
-  // ...
+
+  if((n % 5 === 0) || n === 0){
+    return n
+  }else {
+    for(let i = 0; i <= 5; i++ ){
+        //add one to n
+        n += 1;
+        
+        if(n % 5 === 0){
+            return n;  
+        }  
+    }
+  }
+}
+
+//Optimal Solution
+function optimalRoundToNext5(n){
+    return Math.ceil(n/5) * 5;
 }
