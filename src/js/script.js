f6eac910df8747fa94cd9d76ea4a9a053d0a9bcf
Batubo Victory <victorybatubo76@gmail.com>
2026-01-07 08:40:42 +0100

    refactor: Seperate each code solution into it's individual file

--- src/js/script.js
+++ src/js/script.js
@@ -1,31 +1,4 @@
 
-// Challenge: Split a string by uppercase letters
-// Example: "camelCaseString" becomes "camel Case String"
-// Difficulty: 4/5
-// Ranking: 6 KATA
-
-const solution = str => {
-  let index = []; //to find the index of uppercase letters
-  let parts = []; //to store the parts of the string
-
-  let start = 0; //to keep track of the start index of each part
-
-  
-  str.split('').forEach((x,i) => {
-    if (x === x.toUpperCase()){
-      index.push(i);
-    }
-  })
-  
-  index.forEach(index => {
-    const part = str.slice(start, index)
-    parts.push(part);
-    start = index;
-  });
-
-  parts.push(str.slice(start))
-  return parts.join(' ');
-}
 
 //Implement a function that computes the difference between two lists. The function should remove all occurrences of elements from the first list (a) that are present in the second list (b). The order of elements in the first list should be preserved in the result.
  
