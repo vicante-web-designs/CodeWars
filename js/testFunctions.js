e9c9fcd4fe99a94756944b9d1daaaa213b597a84
Batubo Victory <victorybatubo76@gmail.com>
2026-01-21 09:14:00 +0100

    fix: Optimize solution

--- js/testFunctions.js
+++ js/testFunctions.js
@@ -39,3 +39,28 @@ function fixedGetDivisorsCnt(n){
 
     return count
 }
+
+
+//Problem: It's still inefficient, as it breaks form large counts, or it counts twice
+
+//Perfect Method
+function getDivisorsCnt(n) {
+  let counter = 0;
+
+  if (n % Math.sqrt(n) == 0) {
+    counter++;
+  }// If n is a perfect square, it already has a known divisor
+
+  for (let i = 1; i < Math.sqrt(n); i++) {
+    if (n % i == 0) {
+      counter += 2;
+    }
+  }
+  /*Logs the two ends of a divisor at once... more efficient than doing it one by one.
+  
+  e.g divisors of 8 => 2 * 4 = 8 => log both 2 and 4 at once, instead of counting up to 2 again
+  
+  */
+
+  return counter;
+}
