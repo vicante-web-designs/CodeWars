2fcd745d6656cdbd5b5b7810accbc3db6dc091f1
Batubo Victory <victorybatubo76@gmail.com>
2026-01-27 18:16:05 +0100

    feat: Playing around with async functions and APIs

---
+++ js/testFunctions.js
@@ -0,0 +1,15 @@
+const getApi = async() => {
+
+    try{
+        const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    
+        const data = await res.json()
+
+        console.log(data)
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+getApi()
