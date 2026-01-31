3ff83bcc9fe7f478046b013214f912057500c6b0
Batubo Victory <victorybatubo76@gmail.com>
2026-01-31 11:02:37 +0100

    feat: Log removeUrlAnchor challenge

---
+++ removeUrlAnchor.js
@@ -0,0 +1,16 @@
+/*
+Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+Examples
+"www.codewars.com#about" --> "www.codewars.com"
+"www.codewars.com?page=1" -->"www.codewars.com?page=1"
+*/
+
+function removeUrlAnchor(url){
+
+    return url.includes('#') ? url.slice(0, url.indexOf('#')) : url
+
+}
+
+//Optimized solution
+const removeUrlAnchor2 = url => url.includes('#') ? url.slice(0, url.indexOf('#')) : url
