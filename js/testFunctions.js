b0f7199dfb4c3f477a92dc497c1ed2c656d46530
Batubo Victory <victorybatubo76@gmail.com>
2026-02-03 08:38:09 +0100

    feat: Solved my highest level of kata so far

--- js/testFunctions.js
+++ js/testFunctions.js
@@ -23,25 +23,44 @@ Attention: If the number has leading zeros the amount of digits should be consid
 */
 
 function incrementString (strng) {
-  //loop through the string
-  const splitStrng = strng.split('')
 
-  console.log(splitStrng[splitStrng.length - 3])
+  const splitStrng = strng.split('') //Split the string
+
+  let lastNumArr = []; //Array to store the last set of numbers
+
+  let index; //Variable to store the index of the first number from the last set of numbers
  
+  //Check if the last character on the string is a number
   if(/\d/.test(splitStrng[splitStrng.length - 1])){
-    //loop through the array in reverse 
-    //if it's a number, store in a new array
-    // if not, then break the loop
-    //reverse the array
-    //then join it, turn it into a number, add 1, then padd it with zeros
-    //slice the string from the first char to the last set of numbers
-    //join it with the numbers and return
+   
+     //loop through the array in reverse 
+    for(let i = splitStrng.length - 1; i >= 0 ; i--){
+
+      //check if the character is a number
+      if(/\d/.test(splitStrng[i])){
+
+        lastNumArr.push(splitStrng[i]) //push the character into the array for last set of numbers
+
+        index = i; //At the end of the loop, this should store the first number from the last set of numbers
+
+      } else{
+
+        break; //Let the loop stop once it encounters a letter
+
+      }
+    }
+
+    lastNumArr = lastNumArr.reverse(); //Since we looped and stored in reverse, now we need to reverse it again to get the original order
+
+    const calc = String(Number(lastNumArr.join('')) + 1); //Join the array into a string, turn it into a number, add 1 to it, then convert it back to a string
+
+    const paddedCalc = calc.padStart(lastNumArr.length, '0') //Add the leading zeros that were removed during the calculation
+    
+    const slicedStrng = strng.slice(0,index) //slice the string from the first char to the last set of numbers
+
+    return `${slicedStrng}${paddedCalc}` //join it with the numbers and return
 
   }else{
     return strng += 1; //Just add the string '1' to the end of the string
   }
 }
-
-console.log(incrementString('foo99obar99'));
-
-//TODO: Complete this kata
